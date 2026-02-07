81233de71610d6c2eef2917a5b599f600e7b296d
Alketa2 <ramaalketa2406@gmail.com>
2026-02-07 11:08:56 +0100

    Remove UniversityManagementBackend folder

--- UniversityManagementBackend/UniversityManagement.Application/DTOs/Student/AdmitStudentToProgramDto.cs
+++
@@ -1,8 +0,0 @@
-namespace UniversityManagement.Application.DTOs.Student;
-
-public class AdmitStudentToProgramDto
-{
-    public Guid StudentId { get; set; }
-    public Guid ProgramId { get; set; }
-    public DateTime? AdmissionDate { get; set; }
-}
--- UniversityManagementBackend/UniversityManagement.Infrastructure/Repositories/IExamRepository.cs
+++
@@ -1,9 +0,0 @@
-using UniversityManagement.Domain.Common;     
-using UniversityManagement.Domain.Interfaces; 
-using UniversityManagement.Domain.Entities;   
-namespace UniversityManagement.Infrastructure.Repositories;
-
-public interface IExamRepository : IRepository<Exam>
-{
-    Task<List<Exam>> GetBySubjectIdAsync(Guid subjectId);
-}
